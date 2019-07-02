79f49774d7b920c1c0a59ca7de1e3ff89a9a8a65
Jiajie Chen <noc@jiegec.ac.cn>
2019-07-02 16:55:25 +0800

    Initial rank guesser

---
+++ guesser.py
@@ -0,0 +1,26 @@
+# TODO: read one int from standard input
+# HINT: use input()
+rank = 1
+
+# infinite loop
+while True:
+    # TODO: print guessing message
+    # HINT: use print()
+
+    # TODO: read one int from standard input
+    # HINT: use input()
+    guess = 0
+
+    # TODO: how to compare guess and rank?
+    if guess == 1:
+        # TODO: print correct message
+        # HINT: use print()
+        break
+    elif guess == 2:
+        # TODO: print too big message
+        # HINT: use print()
+        break
+    elif guess == 3:
+        # TODO: print too small message
+        # HINT: use print()
+        break
